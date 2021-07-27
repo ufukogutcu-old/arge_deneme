9866aa7a71b1274076b455bcbfce7b5d8f6bc7e0
ufukogutcu <ufukogutcu@hotmail.com>
2021-07-27 23:00:51 +0300

    Added support for Postgresql

--- deneme/settings.py
+++ deneme/settings.py
@@ -75,12 +75,35 @@ WSGI_APPLICATION = 'deneme.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+postgresql = False
+
+if postgresql:
+    DATABASES = {
+
+        'default': {
+
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+            'NAME': 'mydb',
+
+            'USER': 'postgres',
+
+            'PASSWORD': 'password',
+
+            'HOST': 'localhost',
+
+            'PORT': '5432',
+
+        }
+
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
